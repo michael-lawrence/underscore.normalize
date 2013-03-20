f4123126bf683e5aaf89c981f9a1fea8e80fa2a1
Michael Lawrence <michael.lawrence@rhythmagency.com>
2013-03-20 09:19:08 -0700

    Updating code to use new underscore.mixinTemplate code.

--- underscore.normalize.js
+++ underscore.normalize.js
@@ -1,8 +1,46 @@
 'use strict';
 
-(function () {
-	if (_) { // Underscore.js
-		var normalize = function (original, scaled) {
+(function (_, plugin) {
+	var _private = {
+		'registerMixin' : function (_) {
+			plugin.mixin[plugin.name] = plugin.fn;
+			_.mixin(plugin.mixin);
+		}
+	};
+
+	if (_) { // Global underscore.js
+		plugin.fn = plugin.factory(_);
+		plugin.mixin = {};
+	}
+
+	if (typeof exports !== 'undefined' && plugin.fn) { // CommonJS
+		if (typeof module !== 'undefined' && module.exports) {
+			module.exports = plugin.fn;
+		}
+
+		exports[plugin.name] = plugin.fn;
+	} else if (typeof define === 'function' && define.amd) { // AMD
+		define(['underscore'], function (_) {
+			plugin.fn = plugin.factory(_);
+
+			_private.registerMixin(_);
+
+			return plugin.fn;
+		});
+	} else if (!_) {
+		/*
+			underscore.js isn't defined and user is
+			trying to use mixin directly without
+			CommonJS or AMD support
+		*/
+		throw 'underscore.js not defined.';
+	} else {
+		_private.registerMixin(_);
+	}
+})(window._, {
+	'name' : 'normalize',
+	'factory' : function (_) {
+		return function (original, scaled) {
 			var originalArray, originalMin, originalMax, scaledArray, scaledMin, scaledMax;
 
 			if (_.isArray(original)) {
@@ -30,20 +68,5 @@
 				return scaledMin + (((value - originalMin) * (scaledMax - scaledMin)) / (originalMax - originalMin));
 			});
 		};
-
-		if (typeof exports !== 'undefined') { // CommonJS
-			if (typeof module !== 'undefined' && module.exports) {
-				module.exports = normalize;
-			}
-			exports.normalize = normalize;
-		} else if (typeof define === 'function' && define.amd) { // AMD
-			define('underscore.normalize', [], function() {
-				return normalize;
-			});
-		}
-
-		_.mixin({'normalize' : normalize}); // Underscore.js
-	} else {
-		throw 'Underscore.js not defined.';
 	}
-})();
+});
